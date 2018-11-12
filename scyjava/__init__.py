1c0a48b21eb8b24d1cd1399be1483f3f1e950a4c
Philipp Hanslovsky <hanslovskyp@janelia.hhmi.org>
2018-11-12 14:18:10 -0500

    use jrun to start scyjava

--- scyjava/__init__.py
+++ scyjava/__init__.py
@@ -1,73 +1,52 @@
-def _get_logger(name):
-    import logging
-    return logging.getLogger(name)
+# from .version import __version__ as version
+
+import logging
+import os
+
+_logger = logging.getLogger(__name__)
 
 def _init_jvm():
-
+    import scyjava_config
     import jnius_config
+    import jrun
 
-    import os
+    PYJNIUS_JAR_STR = 'PYJNIUS_JAR'
+    if PYJNIUS_JAR_STR not in globals():
+        try:
+            PYJNIUS_JAR = os.environ[PYJNIUS_JAR_STR]
+            jnius_config.add_classpath(PYJNIUS_JAR)
+        except KeyError as e:
+            print("Path to pyjnius.jar not defined! Use environment variable {} to define it.".format(PYJNIUS_JAR_STR))
+            raise e
 
-    CLASSPATH_STR = 'SCYJAVA_CLASSPATH'
+    endpoints = scyjava_config.get_endpoints()
+    repositories = scyjava_config.get_repositories()
 
-    CLASSPATH = os.environ[CLASSPATH_STR].split(jnius_config.split_char) if CLASSPATH_STR in os.environ else []
+    _logger.debug('Adding jars from endpoints %s', endpoints)
 
-    jnius_config.add_classpath(*CLASSPATH)
+    if len(endpoints) > 0:
+        _, workspace = jrun.resolve_dependencies(
+            '+'.join(endpoints[:1] + sorted(endpoints[1:])),
+            m2_repo=scyjava_config.get_m2_repo(),
+            cache_dir=scyjava_config.get_cache_dir(),
+            repositories=repositories,
+            verbose=scyjava_config.get_verbose()
+        )
+        jnius_config.add_classpath(os.path.join(workspace, '*'))
 
-    JVM_OPTIONS_STR = 'SCYJAVA_JVM_OPTIONS'
+    try:
+        import jnius
+        return jnius
+    except KeyError as e:
+        if e.args[0] == 'JAVA_HOME':
+            _logger.info('Unable to import scyjava: JAVA_HOME environment variable not defined, cannot import jnius.')
+        return None
 
-    if JVM_OPTIONS_STR in os.environ:
-        jnius_config.add_options(*os.environ[JVM_OPTIONS_STR].split(' '))
-
-    import jnius
-
-    scijava = None # jnius.autoclass('some scijava grab class')
-
-    return jnius_config, jnius, scijava
-
-logger = _get_logger('scyjava')
-
-config, _jnius, _scijava  = _init_jvm()
-
-from jnius import autoclass as _autoclass, cast as _cast
-
-# for now, use grape directly:
-_HashMap     = _autoclass('java.util.HashMap')
-_GrapeIvy    = _autoclass('groovy.grape.GrapeIvy')
-_grapeIvy    = _GrapeIvy()
-_classLoader = _autoclass('groovy.lang.GroovyClassLoader')()
-
-def _default_map(**extra_options):
-    m = _HashMap()
-    m.put('classLoader', _classLoader)
-    m.put('autoDownload', 'true')
-    m.put("noExceptions", 'false')
-    for k, v in extra_options.items():
-        m.put(k, v)
-    return m
-
-def repository(url, name=None, **grape_options):
-    m = _default_map(**grape_options)
-    m.put('name', name)
-    m.put('root', url)
-    return _grapeIvy.addResolver(m)
-
-repository('http://maven.imagej.net/content/groups/public', 'imagej')
+jnius = _init_jvm()
+if (jnius == None):
+    raise ImportError('Unable to import scyjava dependency jnius.')
 
 
-def dependency(groupId, artifactId, version=None, **grape_options):
-    m = _default_map(**grape_options)
-    m.put('artifactId', artifactId)
-    m.put('groupId', groupId)
-    m.put('version', version)
-    return _grapeIvy.grab(m)
 
-def import_class(clazz):
-    return _autoclass( clazz )
-
-def list_grapes():
-    grapes = _grapeIvy.enumerateGrapes()
-    grapes = {k : grapes.get(k).toString() for k in grapes.keySet().toArray()}
-    return grapes
 
 
--- setup.py
+++ setup.py
@@ -2,6 +2,13 @@ import setuptools
 
 setuptools.setup(
     name='scyjava',
-    packages=['scyjava']
+    packages=['scyjava'],
+    py_modules=['scyjava_config'],
+    version='0.1.0-dev',
+    author='Philipp Hanslovsky',
+    author_email='hanslovskyp@janelia.hhmi.org',
+    description='scyjava',
+    url='https://github.com/scijava/scyjava',
+    install_requires=['pyjnius', 'jrun']
     )
 

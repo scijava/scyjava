dafb8d69e1f70b61816a29e2ef9bdb3f9a00953a
hinerm <hinerm@gmail.com>
2021-09-23 11:22:08 -0500

    Release version 1.3.1

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name='scyjava',
     python_requires='>=3',
     packages=find_packages(),
-    version="1.3.1.dev0",
+    version="1.3.1",
     author='Curtis Rueden, Philipp Hanslovsky, Edward Evans, Mark Hiner',
     author_email='ctrueden@wisc.edu',
     description='scyjava',

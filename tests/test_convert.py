c91b8c8e7384566977a262ec997599e7a9d63963
Gabriel Selzer <gjselzer@wisc.edu>
2022-04-06 16:00:12 -0500

    Format code using black

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -3,8 +3,9 @@ import unittest
 from jpype import JArray, JInt, JLong
 from scyjava import Converter, config, jclass, jimport, start_jvm, to_java, to_python
 
-config.endpoints.append('org.scijava:scijava-table')
-config.add_option('-Djava.awt.headless=true')
+config.endpoints.append("org.scijava:scijava-table")
+config.add_option("-Djava.awt.headless=true")
+
 
 def assert_same_table(table, df):
     assert len(table.toArray()) == df.shape[1]
@@ -17,42 +18,41 @@ def assert_same_table(table, df):
 
 
 class TestConvert(unittest.TestCase):
-
     def testClass(self):
         """
         Tests class detection from Java objects.
         """
         int_class = jclass(to_java(5))
-        self.assertEqual('java.lang.Integer', int_class.getName())
+        self.assertEqual("java.lang.Integer", int_class.getName())
 
         long_class = jclass(to_java(4000000001))
-        self.assertEqual('java.lang.Long', long_class.getName())
+        self.assertEqual("java.lang.Long", long_class.getName())
 
         bigint_class = jclass(to_java(9879999999999999789))
-        self.assertEqual('java.math.BigInteger', bigint_class.getName())
+        self.assertEqual("java.math.BigInteger", bigint_class.getName())
 
-        string_class = jclass(to_java('foobar'))
-        self.assertEqual('java.lang.String', string_class.getName())
+        string_class = jclass(to_java("foobar"))
+        self.assertEqual("java.lang.String", string_class.getName())
 
         list_class = jclass(to_java([1, 2, 3]))
-        self.assertEqual('java.util.ArrayList', list_class.getName())
+        self.assertEqual("java.util.ArrayList", list_class.getName())
 
-        map_class = jclass(to_java({'a':'b'}))
-        self.assertEqual('java.util.LinkedHashMap', map_class.getName())
+        map_class = jclass(to_java({"a": "b"}))
+        self.assertEqual("java.util.LinkedHashMap", map_class.getName())
 
-        self.assertEqual('java.util.Map', jclass('java.util.Map').getName())
+        self.assertEqual("java.util.Map", jclass("java.util.Map").getName())
 
     def testBoolean(self):
         jt = to_java(True)
         self.assertEqual(True, jt.booleanValue())
         pt = to_python(jt)
         self.assertEqual(True, pt)
-        self.assertEqual('True', str(pt))
+        self.assertEqual("True", str(pt))
         jf = to_java(False)
         self.assertEqual(False, jf.booleanValue())
         pf = to_python(jf)
         self.assertEqual(False, pf)
-        self.assertEqual('False', str(pf))
+        self.assertEqual("False", str(pf))
 
     def testInteger(self):
         i = 5
@@ -79,7 +79,7 @@ class TestConvert(unittest.TestCase):
         self.assertEqual(str(bi), str(pbi))
 
     def testFloat(self):
-        f = 5.
+        f = 5.0
         jf = to_java(f)
         self.assertEqual(f, jf.floatValue())
         pf = to_python(jf)
@@ -87,7 +87,7 @@ class TestConvert(unittest.TestCase):
         self.assertEqual(str(f), str(pf))
 
     def testDouble(self):
-        Float = jimport('java.lang.Float')
+        Float = jimport("java.lang.Float")
         d = Float.MAX_VALUE * 2
         jd = to_java(d)
         self.assertEqual(d, jd.doubleValue())
@@ -96,7 +96,7 @@ class TestConvert(unittest.TestCase):
         self.assertEqual(str(d), str(pd))
 
     def testString(self):
-        s = 'Hello world!'
+        s = "Hello world!"
         js = to_java(s)
         for e, a in zip(s, js.toCharArray()):
             self.assertEqual(e, a)
@@ -105,19 +105,19 @@ class TestConvert(unittest.TestCase):
         self.assertEqual(str(s), str(ps))
 
     def testList(self):
-        l = 'The quick brown fox jumps over the lazy dogs'.split()
+        l = "The quick brown fox jumps over the lazy dogs".split()
         jl = to_java(l)
         for e, a in zip(l, jl):
             self.assertEqual(e, to_python(a))
         pl = to_python(jl)
         self.assertEqual(l, pl)
         self.assertEqual(str(l), str(pl))
-        self.assertEqual(pl[1], 'quick')
-        pl[7] = 'silly'
-        self.assertEqual('The quick brown fox jumps over the silly dogs', ' '.join(pl))
+        self.assertEqual(pl[1], "quick")
+        pl[7] = "silly"
+        self.assertEqual("The quick brown fox jumps over the silly dogs", " ".join(pl))
 
     def testSet(self):
-        s = set(['orange', 'apple', 'pineapple', 'plum'])
+        s = set(["orange", "apple", "pineapple", "plum"])
         js = to_java(s)
         self.assertEqual(len(s), js.size())
         for e in s:
@@ -136,21 +136,21 @@ class TestConvert(unittest.TestCase):
 
     def testDict(self):
         d = {
-            'access_log': [
-                {'stored_proc': 'getsomething'},
-                {'uses': [
-                    {'usedin': 'some->bread->crumb'},
-                    {'usedin': 'something else here'},
-                    {'stored_proc': 'anothersp'}
-                ]},
-                {'uses': [
-                    {'usedin': 'blahblah'}
-                ]}
+            "access_log": [
+                {"stored_proc": "getsomething"},
+                {
+                    "uses": [
+                        {"usedin": "some->bread->crumb"},
+                        {"usedin": "something else here"},
+                        {"stored_proc": "anothersp"},
+                    ]
+                },
+                {"uses": [{"usedin": "blahblah"}]},
             ],
-            'reporting': [
-                {'stored_proc': 'reportingsp'},
-                {'uses': [{'usedin': 'breadcrumb'}]}
-            ]
+            "reporting": [
+                {"stored_proc": "reportingsp"},
+                {"uses": [{"usedin": "breadcrumb"}]},
+            ],
         }
         jd = to_java(d)
         self.assertEqual(len(d), jd.size())
@@ -163,12 +163,12 @@ class TestConvert(unittest.TestCase):
         self.assertEqual(str(d), str(pd))
 
     def testMixed(self):
-        d = {'a':'b', 'c':'d'}
-        l = ['e', 'f', 'g', 'h']
-        s = set(['i', 'j', 'k'])
+        d = {"a": "b", "c": "d"}
+        l = ["e", "f", "g", "h"]
+        s = set(["i", "j", "k"])
 
         # mixed types in a dictionary
-        md = {'d': d, 'l': l, 's': s, 'str': 'hello'}
+        md = {"d": d, "l": l, "s": s, "str": "hello"}
         jmd = to_java(md)
         self.assertEqual(len(md), jmd.size())
         for k, v in md.items():
@@ -180,7 +180,7 @@ class TestConvert(unittest.TestCase):
         self.assertEqual(str(md), str(pmd))
 
         # mixed types in a list
-        ml = [d, l, s, 'hello']
+        ml = [d, l, s, "hello"]
         jml = to_java(ml)
         for e, a in zip(ml, jml):
             self.assertEqual(e, to_python(a))
@@ -189,17 +189,17 @@ class TestConvert(unittest.TestCase):
         self.assertEqual(str(ml), str(pml))
 
     def testNone(self):
-        d = {'key':None, None:'value', 'foo':'bar'}
+        d = {"key": None, None: "value", "foo": "bar"}
         jd = to_java(d)
         self.assertEqual(3, jd.size())
-        self.assertEqual(None, jd.get('key'))
-        self.assertEqual('value', jd.get(None))
-        self.assertEqual('bar', jd.get('foo'))
+        self.assertEqual(None, jd.get("key"))
+        self.assertEqual("value", jd.get(None))
+        self.assertEqual("bar", jd.get("foo"))
         pd = to_python(jd)
         self.assertEqual(d, pd)
 
     def testGentle(self):
-        Object = jimport('java.lang.Object')
+        Object = jimport("java.lang.Object")
         unknown_thing = Object()
         converted_thing = to_python(unknown_thing, gentle=True)
         assert type(converted_thing) == Object
@@ -213,48 +213,50 @@ class TestConvert(unittest.TestCase):
 
     def testStructureWithSomeUnsupportedItems(self):
         # Create Java data structure with some challenging items.
-        Object = jimport('java.lang.Object')
-        jmap = to_java({
-            'list': ['a', Object(), 1],
-            'set': {'x', Object(), 2},
-            'object': Object(),
-            'foo': 'bar'
-        })
-        self.assertEqual('java.util.LinkedHashMap', jclass(jmap).getName())
+        Object = jimport("java.lang.Object")
+        jmap = to_java(
+            {
+                "list": ["a", Object(), 1],
+                "set": {"x", Object(), 2},
+                "object": Object(),
+                "foo": "bar",
+            }
+        )
+        self.assertEqual("java.util.LinkedHashMap", jclass(jmap).getName())
 
         # Convert it back to Python.
         pdict = to_python(jmap)
-        l = pdict['list']
-        self.assertEqual(pdict['list'][0], 'a')
-        assert type(pdict['list'][1]) == Object
-        assert pdict['list'][2] == 1
-        assert 'x' in pdict['set']
-        assert 2 in pdict['set']
-        assert len(pdict['set']) == 3
-        assert type(pdict['object']) == Object
-        self.assertEqual(pdict['foo'], 'bar')
-
+        l = pdict["list"]
+        self.assertEqual(pdict["list"][0], "a")
+        assert type(pdict["list"][1]) == Object
+        assert pdict["list"][2] == 1
+        assert "x" in pdict["set"]
+        assert 2 in pdict["set"]
+        assert len(pdict["set"]) == 3
+        assert type(pdict["object"]) == Object
+        self.assertEqual(pdict["foo"], "bar")
 
     def test_conversion_priority(self):
         # Add a converter prioritized over the default converter
-        String = jimport('java.lang.String')
-        invader = 'Not Hello World'
+        String = jimport("java.lang.String")
+        invader = "Not Hello World"
 
         from scyjava import add_java_converter
+
         add_java_converter(
             Converter(
                 predicate=lambda obj: isinstance(obj, str),
-                converter=lambda obj: String(invader.encode('utf-8'), 'utf-8'),
-                priority=100
+                converter=lambda obj: String(invader.encode("utf-8"), "utf-8"),
+                priority=100,
             )
         )
 
         # Ensure that the conversion uses our new converter
-        s = 'Hello world!'
+        s = "Hello world!"
         js = to_java(s)
         for e, a in zip(invader, js.toCharArray()):
             self.assertEqual(e, a)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()

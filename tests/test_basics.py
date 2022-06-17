17341a37c2681959c6eb8bd930ea35c0a8be0776
Curtis Rueden <ctrueden@wisc.edu>
2022-06-17 12:56:57 -0500

    Add some basic tests: jclass, jimport, jinstance

---
+++ tests/test_basics.py
@@ -0,0 +1,45 @@
+import re
+
+import scyjava
+
+
+class TestBasics(object):
+    """
+    Tests basic scyjava functions.
+    """
+
+    def test_jclass(self):
+        """
+        Tests the jclass function.
+        """
+        c = scyjava.jclass("java.lang.Object")
+        assert scyjava.jinstance(c, "java.lang.Class")
+        assert str(c.toString()) == "class java.lang.Object"
+
+    def test_jimport(self):
+        """
+        Tests the jimport function.
+        """
+        Object = scyjava.jimport("java.lang.Object")
+        assert Object is not None
+        assert str(Object)
+        o = Object()
+        assert scyjava.jinstance(o, "java.lang.Object")
+        assert re.match("java.lang.Object@[0-9a-f]{8}", str(o.toString()))
+
+    def test_jinstance(self):
+        """
+        Tests the jinstance function.
+        """
+        jstr = scyjava.to_java("Hello")
+        assert scyjava.jinstance(jstr, "java.lang.String")
+
+        jint = scyjava.to_java(5)
+        assert scyjava.jinstance(jint, "java.lang.Integer")
+
+        jfloat = scyjava.to_java(3.5)
+        assert scyjava.jinstance(jfloat, "java.lang.Float")
+
+        jlist = scyjava.to_java([3, 2, 1])
+        assert scyjava.jinstance(jlist, "java.util.List")
+        assert scyjava.jinstance(jlist, "java.util.ArrayList")

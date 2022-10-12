4990a8508676131ae00c1ba5c06db5097b45df62
Curtis Rueden <ctrueden@wisc.edu>
2022-10-12 11:59:25 -0500

    Add type reasoning utility methods for arraylikes
    These are mostly useful for NumPy-related activities.
    But they do not introduce any numpy dependency, so we
    include them as utility methods here for convenience.

--- src/scyjava/__init__.py
+++ src/scyjava/__init__.py
@@ -340,7 +340,7 @@ def when_jvm_stops(f):
     _shutdown_callbacks.append(f)
 
 
-# -- Utility functions --
+# -- Version reasoning --
 
 
 def get_version(java_class_or_python_package):
@@ -407,7 +407,58 @@ def compare_version(version, java_class_version):
     )
 
 
-# -- Type Conversion Utilities --
+# -- Type reasoning --
+
+
+def is_arraylike(arr):
+    """
+    Return True iff the object is arraylike: possessing
+    .shape, .dtype, .__array__, and .ndim attributes.
+
+    :param arr: The object to check for arraylike properties
+    :return: True iff the object is arraylike
+    """
+    return (
+        hasattr(arr, "shape")
+        and hasattr(arr, "dtype")
+        and hasattr(arr, "__array__")
+        and hasattr(arr, "ndim")
+    )
+
+
+def is_memoryarraylike(arr):
+    """
+    Return True iff the object is memoryarraylike:
+    an arraylike object whose .data type is memoryview.
+
+    :param arr: The object to check for memoryarraylike properties
+    :return: True iff the object is memoryarraylike
+    """
+    return (
+        is_arraylike(arr)
+        and hasattr(arr, "data")
+        and type(arr.data).__name__ == "memoryview"
+    )
+
+
+def is_xarraylike(xarr):
+    """
+    Return True iff the object is xarraylike:
+    possessing .values, .dims, and .coords attributes,
+    and whose .values are arraylike.
+
+    :param arr: The object to check for xarraylike properties
+    :return: True iff the object is xarraylike
+    """
+    return (
+        hasattr(xarr, "values")
+        and hasattr(xarr, "dims")
+        and hasattr(xarr, "coords")
+        and is_arraylike(xarr.values)
+    )
+
+
+# -- Type conversion --
 
 # NB: We cannot use org.scijava.priority.Priority or other Java-side class
 # here because we don't want to impose Java-side dependencies, and we don't

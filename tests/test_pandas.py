b365ed0769b06e82299871c8c597370c43ccda2f
Edward Evans <elevans2@wisc.edu>
2022-11-09 11:41:03 -0600

    Add tests for Table to Pandas conversion
    This commit adds tests for 74e66bfd923a98746a11b3d3f8510eb6496f4218
    which fixes scijava table -> pandas dataframe conversion priority.
    We did not catch this bug because we were only testing one direction
    (Pandas to SciJava Table).

--- tests/test_pandas.py
+++ tests/test_pandas.py
@@ -1,8 +1,9 @@
 import numpy as np
 import numpy.testing as npt
 import pandas as pd
+from jpype import JBoolean, JFloat, JInt, JString
 
-from scyjava import config, jinstance, to_java
+from scyjava import config, jimport, jinstance, to_java, to_python
 
 config.endpoints.append("org.scijava:scijava-table")
 config.add_option("-Djava.awt.headless=true")
@@ -69,3 +70,76 @@ class TestPandas(object):
         # Table types cannot be the same here, unless we want to cast.
         # assert_same_table(table, df)
         assert jinstance(table, "org.scijava.table.DefaultGenericTable")
+
+    def testTabletoPandas(self):
+        # Float table
+        table = jimport("org.scijava.table.DefaultFloatTable")()
+        table.appendColumns(["header1", "header2", "header3", "header4", "header5"])
+        table.setRowCount(7)
+        array = np.random.random(size=(7, 5))
+
+        table = self._fill_table(table, array, JFloat)
+        df = to_python(table)
+
+        assert_same_table(table, df)
+        for col in df.columns:
+            assert df.dtypes[col] == np.float64
+
+        # Int table
+        table = jimport("org.scijava.table.DefaultIntTable")()
+        table.appendColumns(["header1", "header2", "header3", "header4", "header5"])
+        table.setRowCount(7)
+        array = np.random.random(size=(7, 5)) * 100
+        array = array.astype("int32")
+
+        table = self._fill_table(table, array, JInt)
+        df = to_python(table)
+
+        assert_same_table(table, df)
+        for col in df.columns:
+            assert df.dtypes[col] == np.int64
+
+        # Bool table
+        table = jimport("org.scijava.table.DefaultBoolTable")()
+        table.appendColumns(["header1", "header2", "header3", "header4", "header5"])
+        table.setRowCount(7)
+        array = np.random.random(size=(7, 5)) > 0.5
+
+        table = self._fill_table(table, array, JBoolean)
+        df = to_python(table)
+
+        assert_same_table(table, df)
+        for col in df.columns:
+            assert df.dtypes[col] == np.bool8
+
+        # Mixed table
+        table = jimport("org.scijava.table.DefaultGenericTable")()
+        table.appendColumns(["header1", "header2", "header3", "header4"])
+        table.setRowCount(7)
+        array_float = np.random.random(size=(7, 1))
+        array_int = np.random.random(size=(7, 1)) * 100
+        array_int = array_int.astype("int32")
+        array_bool = np.random.random(size=(7, 1)) > 0.5
+        array_str = np.array(["foo", "bar", "foobar", "barfoo", "oofrab", "oof", "rab"])
+
+        # fill mixed table
+        for i in range(table.getRowCount()):
+            table.set(0, i, JFloat(array_float[i]))
+            table.set(1, i, JInt(array_int[i].item()))
+            table.set(2, i, JBoolean(array_bool[i]))
+            table.set(3, i, JString(array_str[i]))
+
+        df = to_python(table)
+        # Table types cannot be the same here, unless we want to cast.
+        # assert_same_table(table, df)
+        assert type(df["header1"][0]) == float
+        assert type(df["header2"][0]) == int
+        assert type(df["header3"][0]) == bool
+        assert type(df["header4"][0]) == str
+
+    def _fill_table(self, table, ndarr, type):
+        for i in range(table.getColumnCount()):
+            s = ndarr[:, i]
+            for j in range(table.getRowCount()):
+                table.setValue(i, j, type(s[j]))
+        return table

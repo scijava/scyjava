02984cd6dbf712f77b121554d1e899a65976cf1d
Curtis Rueden <ctrueden@wisc.edu>
2023-11-21 14:42:55 -0600

    Make JVM version test an integration test
    It needs to run in an isolated Python+Java process so that
    we can truly validate the before-and-after of version detection.
    Otherwise, the test simply checks the same thing twice, which is
    version reporting *after* the JVM is initialized.

---
+++ tests/it/jvm_version.py
@@ -0,0 +1,22 @@
+"""
+Test the jvm_version() function.
+"""
+
+import scyjava
+
+assert not scyjava.jvm_started()
+
+before_version = scyjava.jvm_version()
+assert before_version is not None
+assert len(before_version) >= 3
+assert before_version[0] > 0
+
+scyjava.config.add_option("-Djava.awt.headless=true")
+scyjava.start_jvm()
+
+after_version = scyjava.jvm_version()
+assert after_version is not None
+assert len(after_version) >= 3
+assert after_version[0] > 0
+
+assert before_version == after_version

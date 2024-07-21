a1e48da56007ee98d70aab2db9e2f5a4cde82e2b
jschneidereit <schneidereit@pm.me>
2024-07-20 17:28:23 -0700

    chore: refactor java_heap tests for readable errors

--- tests/it/java_heap.py
+++ tests/it/java_heap.py
@@ -1,34 +1,28 @@
 """
-Test scyjava JVM memory-related functions.
-"""
+ Test scyjava JVM memory-related functions.
+ """
+ import math
+ from assertpy import assert_that
 
-import scyjava
+ import scyjava
 
-mb_initial = 50  # initial MB of memory to snarf up
+ mb_initial = 50  # initial MB of memory to snarf up
 
-scyjava.config.set_heap_min(mb=mb_initial)
-scyjava.config.set_heap_max(gb=1)
+ scyjava.config.set_heap_min(mb=mb_initial)
+ scyjava.config.set_heap_max(gb=1)
 
-assert not scyjava.jvm_started()
-scyjava.start_jvm()
+ assert not scyjava.jvm_started()
+ scyjava.start_jvm()
 
-assert scyjava.available_processors() >= 1
+ assert scyjava.available_processors() >= 1
 
-mb_max = scyjava.memory_max() // 1024 // 1024
-mb_total = scyjava.memory_total() // 1024 // 1024
-mb_used = scyjava.memory_used() // 1024 // 1024
+ mb_max = scyjava.memory_max() // 1024 // 1024
+ mb_total = scyjava.memory_total() // 1024 // 1024
+ mb_used = scyjava.memory_used() // 1024 // 1024
 
-# Used memory should be less than the current memory total,
-# which should be less than the maximum heap size.
-assert mb_used <= mb_total <= mb_max, f"{mb_used=} {mb_total=} {mb_max=}"
+ assert_that(mb_used, 'Used memory should be less than the current memory total').is_less_than_or_equal_to(mb_total)
+ assert_that(mb_total, 'current memory total should be less than maximum memory').is_less_than_or_equal_to(mb_max)
+ assert_that(mb_max, 'maximum heap size should be approx. 1 GB').is_between(900, 1024)
 
-# The maximum heap size should be approximately 1 GB.
-assert 900 <= mb_max <= 1024, f"{mb_max=}"
-
-# Most of that memory should still be free; i.e.,
-# we should not be using more than a few MB yet.
-assert mb_used <= 5, f"{mb_used=}"
-
-# The total MB available to Java at this moment
-# should be close to our requested initial amount.
-assert abs(mb_total - mb_initial) < 5, f"{mb_total=} {mb_initial=}"
+ assert_that(mb_used, 'most memory should be available').is_less_than(5)
+ assert_that(mb_total, 'total memory should be close to initial').is_close_to(mb_initial, tolerance=5)

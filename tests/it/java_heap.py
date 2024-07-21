56df79931bdc584d2fc33138652a74d084f78278
jschneidereit <schneidereit@pm.me>
2024-07-20 17:29:18 -0700

    fix: loosen tolerance for comparing memory consumption

--- tests/it/java_heap.py
+++ tests/it/java_heap.py
@@ -1,28 +1,35 @@
 """
- Test scyjava JVM memory-related functions.
- """
- import math
- from assertpy import assert_that
+Test scyjava JVM memory-related functions.
+"""
+import math
+from assertpy import assert_that
 
- import scyjava
+import scyjava
 
- mb_initial = 50  # initial MB of memory to snarf up
 
- scyjava.config.set_heap_min(mb=mb_initial)
- scyjava.config.set_heap_max(gb=1)
+def magnitude(x: int) -> int:
+    return math.floor(math.log10(abs(x)))
 
- assert not scyjava.jvm_started()
- scyjava.start_jvm()
 
- assert scyjava.available_processors() >= 1
+mb_initial = 50  # initial MB of memory to snarf up
 
- mb_max = scyjava.memory_max() // 1024 // 1024
- mb_total = scyjava.memory_total() // 1024 // 1024
- mb_used = scyjava.memory_used() // 1024 // 1024
+scyjava.config.set_heap_min(mb=mb_initial)
+scyjava.config.set_heap_max(gb=1)
 
- assert_that(mb_used, 'Used memory should be less than the current memory total').is_less_than_or_equal_to(mb_total)
- assert_that(mb_total, 'current memory total should be less than maximum memory').is_less_than_or_equal_to(mb_max)
- assert_that(mb_max, 'maximum heap size should be approx. 1 GB').is_between(900, 1024)
+assert not scyjava.jvm_started()
+scyjava.start_jvm()
 
- assert_that(mb_used, 'most memory should be available').is_less_than(5)
- assert_that(mb_total, 'total memory should be close to initial').is_close_to(mb_initial, tolerance=5)
+assert scyjava.available_processors() >= 1
+
+mb_max = scyjava.memory_max() // 1024 // 1024
+mb_total = scyjava.memory_total() // 1024 // 1024
+mb_used = scyjava.memory_used() // 1024 // 1024
+
+assert_that(mb_used, 'Used memory should be less than the current memory total').is_less_than_or_equal_to(mb_total)
+assert_that(mb_total, 'current memory total should be less than maximum memory').is_less_than_or_equal_to(mb_max)
+assert_that(mb_max, 'maximum heap size should be approx. 1 GB').is_between(900, 1024)
+
+tolerance = pow(10, magnitude(mb_initial))
+
+assert_that(mb_used, 'most memory should be available').is_less_than(tolerance)
+assert_that(mb_total, 'total memory should be close to initial').is_close_to(mb_initial, tolerance=tolerance)

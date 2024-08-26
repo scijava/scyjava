ec34f70e019fd6b0710c5cc4fa7fea92375c0487
Edward Evans <elevans2@wisc.edu>
2024-08-26 15:41:53 -0500

    Add validate-pyproject back to lint checks

--- bin/lint.sh
+++ bin/lint.sh
@@ -8,4 +8,6 @@ ruff check
 code=$?; test $code -eq 0 || exitCode=$code
 ruff format --check
 code=$?; test $code -eq 0 || exitCode=$code
+validate-pyproject pyproject.toml
+code=$?; test $code -eq 0 || exitCode=$code
 exit $exitCode
